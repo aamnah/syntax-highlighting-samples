43fdc2cc2b3a8a9cd3ea550c8a88a78d2df1c5cf
Amna <akram.aamnah@gmail.com>
2023-12-27 13:30:56 +0200

    regex

---
+++ languages/regex.ts
@@ -0,0 +1,7 @@
+const url_pattern_https = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+
+const url_pattern = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
+
+const phone_number_pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+
+const regex = new RegExp(url_pattern)
